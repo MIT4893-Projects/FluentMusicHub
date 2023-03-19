e4e024990175467659767eee0e23617261fce7d9
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-03-19 08:51:50 +0700

    Add ISidebarNavigationItem

---
+++ FluentMusicHub/Views/Sidebar/ISidebarNavigationItem.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentMusicHub.Views.Sidebar
+{
+    interface ISidebarNavigationItem
+    {
+        public event RoutedEventHandler SelectingChanged;
+        public bool Selecting { get; set; }
+        public void SelectingChangedHandler(object sender, RoutedEventArgs e);
+    }
+}
