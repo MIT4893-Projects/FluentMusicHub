55a69b4d6be1e158e699f351da07961f44083c87
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-03-19 08:50:40 +0700

    Add HomeButtonView
     - Implement HomeButtonView from UserControl.
     - Implement HomeButtonView from ISidebarNavigationButton.
     - Change button's image on selecting.

---
+++ FluentMusicHub/Views/Sidebar/HomeButtonView.xaml.cs
@@ -0,0 +1,57 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace FluentMusicHub.Views.Sidebar
+{
+    public sealed partial class HomeButtonView : UserControl, ISidebarNavigationItem
+    {
+        private bool SelectingProperty = false;
+        public bool Selecting
+        {
+            get
+            {
+                return SelectingProperty;
+            }
+            set
+            {
+                SelectingProperty = value;
+                SelectingChanged?.Invoke(this, new());
+            }
+        }
+
+        public event RoutedEventHandler SelectingChanged;
+
+        public HomeButtonView()
+        {
+            this.InitializeComponent();
+
+            SelectingChanged += SelectingChangedHandler;
+        }
+
+        public void SelectingChangedHandler(object sender, RoutedEventArgs e)
+        {
+            Uri unselectingImageSource = new("ms-appx:///Assets/home-outline-size-48-white.png");
+            Uri selectingImageSource = new("ms-appx:///Assets/home-fill-size-48-white.png");
+            ButtonImage.Source = new BitmapImage(Selecting ? selectingImageSource : unselectingImageSource);
+        }
+    }
+}
