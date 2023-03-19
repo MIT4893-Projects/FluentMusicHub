b5aeaaa858cf500c873e7e99001ab59fbabc94c4
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-03-19 08:55:03 +0700

    Add Assets image files into project

---
+++ FluentMusicHub/Models/Playlist.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Midi;
+
+namespace FluentMusicHub.Models
+{
+    class Playlist
+    {
+        /// <summary>
+        /// This container stores all the tracks this playlist contains.
+        /// </summary>
+        public readonly List<Track> TrackList = new();
+
+        /// <summary>
+        /// Name of this playlist.
+        /// </summary>
+        public readonly string Name = "";
+
+        /// <summary>
+        /// Create a new empty playlist with its name.
+        /// </summary>
+        /// <param name="name">Name of the playlist.</param>
+        public Playlist(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Create a new playlist with its name and an initialized list of tracks.
+        /// </summary>
+        /// <param name="name">Name of the playlist.</param>
+        /// <param name="trackList">Initialized list of tracks to copy.</param>
+        public Playlist(string name, List<Track> trackList) : this(name)
+        {
+            TrackList = trackList.GetRange(0, trackList.Count);
+        }
+
+        /// <summary>
+        /// Add a track to this playlist.
+        /// </summary>
+        /// <param name="track"></param>
+        public void AddTrack(Track track)
+        {
+            TrackList.Add(track);
+        }
+
+        /// <summary>
+        /// Remove a track object from this playlist.
+        /// </summary>
+        /// <param name="track"></param>
+        public void RemoveTrack(Track track)
+        {
+            TrackList.Remove(track);
+        }
+
+        /// <summary>
+        /// Remove a track object from this playlist with its index.
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveTrack(int index)
+        {
+            TrackList.RemoveAt(index);
+        }
+    }
+}
---
+++ FluentMusicHub/Models/Track.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentMusicHub.Models
+{
+    class Track
+    {
+        /// <summary>
+        /// Artist's informations about this track.
+        /// </summary>
+        public readonly string Artist;
+
+        /// <summary>
+        /// Title of this track (track's name).
+        /// </summary>
+        public readonly string Title;
+
+        /// <summary>
+        /// Create a new track with a title and an artist name.
+        /// </summary>
+        /// <param name="title">Track's title.</param>
+        /// <param name="artist">Track's artist name.</param>
+        public Track(string title, string artist = "")
+        {
+            Artist = artist;
+            Title = title;
+        }
+    }
+}
